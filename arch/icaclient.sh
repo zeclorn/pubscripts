21e797820bd524485f887919c5f94a0db8d09bd6
zeclorn <zeclorn@gmail.com>
2022-02-28 10:09:06 -0600

    Create icaclient.sh

---
+++ arch/icaclient.sh
@@ -0,0 +1,3 @@
+#Run once per user account after installing the ICA Client
+mkdir -p $HOME/.ICAClient/cache
+cp /opt/Citrix/ICAClient/config/{All_Regions,Trusted_Region,Unknown_Region,canonicalization,regions}.ini $HOME/.ICAClient/
